5bcfc62e03fe655168f219b27624598b46709af9
typidoyun <typidoyun@gmail.com>
2023-10-24 10:57:54 +0900

    Add: user.entity.ts

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post("/sign-up")
+    @HttpCode(HttpStatus.OK)
+    public signUp(signUpDto: SignUpDto) {
+
+    }
+}
